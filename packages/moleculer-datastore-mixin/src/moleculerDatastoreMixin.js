590b1952c4d6459da16d6e9bee2b585253fddfcb
Anil Babu Gullapalli <mailaneel@gmail.com>
2018-02-28 14:34:00 +1100

    - added configurable options to enable/disable actions, change actionName - changed adapter name to datastore to avoid confusion

--- packages/moleculer-datastore-mixin/src/moleculerDatastoreMixin.js
+++ packages/moleculer-datastore-mixin/src/moleculerDatastoreMixin.js
@@ -5,92 +5,112 @@ import removeAction from './removeActionMixin'
 import getAction from './getActionMixin'
 import findAction from './findActionMixin'
 
+const availableActions = {
+  create: createAction,
+  insert: insertAction,
+  update: updateAction,
+  remove: removeAction,
+  get: getAction,
+  find: findAction
+}
+
 /**
- * @param {Object} config
+ * @param {object} [config]
+ * @param {boolean} [config.disableActions] true => disable actions
+ * @param {boolean|string} config.create false disable action, user.create => will be action name
+ * @param {boolean|string} config.insert false disable action, user.insert => will be action name
+ * @param {boolean|string} config.update false disable action, user.update => will be action name
+ * @param {boolean|string} config.remove false disable action, user.remove => will be action name
+ * @param {boolean|string} config.get false disable action, user.get => will be action name
+ * @param {boolean|string} config.find false disable action, user.find => will be action name
  */
-const moleculerDatastoreMixin = (config = {}) => {
-  const create = createAction()
-  const insert = insertAction()
-  const update = updateAction()
-  const remove = removeAction()
-  const get = getAction()
-  const find = findAction()
+const moleculerDatastoreMixin = (config) => {
+  const configWithDefaults = Object.assign({
+    disableActions: false
+  }, config)
+
+  let actions = {}
+
+  // if all actions are not disabled
+  if (!configWithDefaults.disableActions) {
+    Object.keys(availableActions).forEach((availableActionName) => {
+      // if action is not disabled
+      if (configWithDefaults[availableActionName] !== false) {
+        // use action name if given
+        const actionName = configWithDefaults[availableActionName] || availableActionName
+        actions[actionName] = availableActions[availableActionName](actionName)
+      }
+    })
+  }
 
   return {
-    actions: {
-      [create.actionName]: create,
-      [insert.actionName]: insert,
-      [update.actionName]: update,
-      [remove.actionName]: remove,
-      [get.actionName]: get,
-      [find.actionName]: find
-    },
+    actions,
     methods: {
-      getAdapter () {
-        return this.adapter
+      getDatastore () {
+        return this.datastore
       },
 
-      async invokeAdapterMethod (method, ctx, ...rest) {
-        const adapter = this.getAdapter()
+      async invokeDatastoreMethod (method, ctx, ...rest) {
+        const adapter = this.getDatastore()
         const options = rest.pop()
         const mergedOptions = Object.assign({}, ctx.meta, options)
         return adapter[method](...rest, mergedOptions)
       },
 
       async insertOne (ctx, entity, options) {
-        return this.invokeAdapterMethod('insertOne', ctx, entity, options)
+        return this.invokeDatastoreMethod('insertOne', ctx, entity, options)
       },
 
       async insertMany (ctx, entities, options) {
-        return this.invokeAdapterMethod('insertMany', ctx, entities, options)
+        return this.invokeDatastoreMethod('insertMany', ctx, entities, options)
       },
 
       async updateById (ctx, id, update, options) {
-        return this.invokeAdapterMethod('updateById', ctx, id, update, options)
+        return this.invokeDatastoreMethod('updateById', ctx, id, update, options)
       },
 
       async updateMany (ctx, filter, update, options) {
-        return this.invokeAdapterMethod('updateMany', ctx, filter, update, options)
+        return this.invokeDatastoreMethod('updateMany', ctx, filter, update, options)
       },
 
       async deleteById (ctx, id, options) {
-        return this.invokeAdapterMethod('deleteById', ctx, id, options)
+        return this.invokeDatastoreMethod('deleteById', ctx, id, options)
       },
 
       async deleteByIds (ctx, ids, options) {
-        return this.invokeAdapterMethod('deleteByIds', ctx, ids, options)
+        return this.invokeDatastoreMethod('deleteByIds', ctx, ids, options)
       },
 
       async deleteMany (ctx, filter, options) {
-        return this.invokeAdapterMethod('deleteMany', ctx, filter, options)
+        return this.invokeDatastoreMethod('deleteMany', ctx, filter, options)
       },
 
       async findById (ctx, id, options) {
-        return this.invokeAdapterMethod('findById', ctx, id, options)
+        return this.invokeDatastoreMethod('findById', ctx, id, options)
       },
 
       async findByIds (ctx, ids, options) {
-        return this.invokeAdapterMethod('findByIds', ctx, ids, options)
+        return this.invokeDatastoreMethod('findByIds', ctx, ids, options)
       },
 
       async findOne (ctx, filter, options) {
-        return this.invokeAdapterMethod('findOne', ctx, filter, options)
+        return this.invokeDatastoreMethod('findOne', ctx, filter, options)
       },
 
       async find (ctx, filter, options) {
-        return this.invokeAdapterMethod('find', ctx, filter, options)
+        return this.invokeDatastoreMethod('find', ctx, filter, options)
       },
 
       async count (ctx, filter, options) {
-        return this.invokeAdapterMethod('count', ctx, filter, options)
+        return this.invokeDatastoreMethod('count', ctx, filter, options)
       }
     },
     created () {
-      if (!this.schema.adapter) {
-        throw new Error('adapter is missing in service schema')
+      if (!this.schema.datastore) {
+        throw new Error('datastore is missing in service schema')
       }
 
-      this.adapter = this.schema.adapter
+      this.datastore = this.schema.datastore
     }
   }
 }
