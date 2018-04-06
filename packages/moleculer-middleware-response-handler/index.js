50e7cd396089768f2b999ae3895a2e4801ed6d43
Anil Gullapalli <mailaneel@gmail.com>
2018-04-06 14:27:55 +1000

    merged response handler, error handler and error mapper

--- packages/moleculer-middleware-response-handler/index.js
+++ packages/moleculer-middleware-response-handler/index.js
@@ -1,29 +1,65 @@
-const has = require('lodash/has')
-
-const createErrorResponse = (error) => {
-  return {
-    success: false,
-    error: error.type || error.name,
-    data: error.data
+const createErrorResponse = (config, action, error, level = 1) => {
+  if (
+    !error.type &&
+    config.mapEntityNotFoundToService !== false &&
+    error.name === 'EntityNotFoundError'
+  ) {
+    error.type = `${action.service.name}.not_found`
   }
+
+  if (level > 1) {
+    return error
+  }
+
+  const errorResponse = {
+    code: error.code,
+    error: !error.code || (error.code && error.code >= 500) ? 'internal_error' : (error.type || error.name),
+    data: error.data ? error.data : undefined,
+    message: error.message ? error.message : undefined
+  }
+
+  if (action.errorMap && action.errorMap[errorResponse.error]) {
+    errorResponse.error = action.errorMap[errorResponse.error]
+  } else if (config.errorMap && config.errorMap[errorResponse.error]) {
+    errorResponse.error = config.errorMap[errorResponse.error]
+  }
+
+  return errorResponse
 }
 
-const createSuccessResponse = (response) => {
-  return {
-    success: true,
-    data: response
-  }
-}
+const createSuccessResponse = (response) => response
 
-const actionResponseHandlerMiddlewareCreator = () => (handler, action) => {
+/**
+ *
+ * Example:
+ *
+ * // global
+ * errorMap: {
+ *   EntityNotFoundError: 'not_found',
+ *   ValidationError: 'validation_error'
+ * }
+ *
+ * action can have same config which will override the global config
+ * errorMap: {
+ *   EntityNotFoundError: 'user_not_found'
+ * }
+ *
+ * @param {Object} [config]
+ * @param {Object} config.errorMap
+ * @param {boolean} [config.mapEntityNotFoundToService]
+ */
+const actionResponseHandlerMiddlewareCreator = (config = {}) => (handler, action) => {
   return (ctx) => {
     return handler(ctx)
-      .then((response) => {
-      // if its already converted to response structure else where do not convert again
-        return !has(response, 'success') ? createSuccessResponse(response) : response
-      }).catch((e) => {
-      // if its already converted to response structure else where do not convert again
-        return !has(e, 'success') ? createErrorResponse(e) : e
+      .then((response) => createSuccessResponse(response))
+      .catch((e) => {
+        const errorResponse = createErrorResponse(config, action, e, ctx.level)
+
+        if (!errorResponse.code || (errorResponse.code && errorResponse.code >= 500)) {
+          ctx.broker.logger.error(e)
+        }
+
+        return ctx.level > 1 ? Promise.reject(errorResponse) : Promise.resolve(errorResponse)
       })
   }
 }
