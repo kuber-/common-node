2c718eae5da593a073112fe234a9eb4f77b51989
Anil Gullapalli <mailaneel@gmail.com>
2018-04-18 16:32:53 +1000

    WIP knex adapter

---
+++ packages/datastore-adapter-knex/src/KnexDatastoreAdapter.js
@@ -0,0 +1,115 @@
+import knex from 'knex'
+
+export default class KnexDataStoreAdapter {
+  /**
+   * @param {object} options
+   * @param {string} [options.tableName]
+   * @param {object} [options.knexOptions]
+   */
+  constructor (options = {}) {
+    this.options = options
+  }
+
+  async init ({ schema }) {
+    const { knexOptions, tableName } = this.options
+    this.schema = schema
+    this.tableName = tableName || schema.name
+    this.db = knex(knexOptions)
+  }
+
+  /**
+   * If transaction is present in options returns transaction specific db object
+   *
+   * @param {*} options
+   */
+  getDBFromOptions (options) {
+    return options && options.tx ? options.tx(this.tableName) : this.db(this.tableName)
+  }
+
+  async insertOne (doc, options) {
+    return this.getDBFromOptions(options).insert(doc, '*').then(rows => rows[0])
+  }
+
+  async insertMany (docs, options) {
+    return this.getDBFromOptions(options).insert(docs, '*')
+  }
+
+  async updateById (id, update, options) {
+    return this.getDBFromOptions(options).where('id', id).update(update)
+  }
+
+  async updateMany (filter, update, options) {
+    return this.getDBFromOptions(options).where(filter).update(update)
+  }
+
+  async deleteById (id, options) {
+    return this.getDBFromOptions(options).where('id', id).delete()
+  }
+
+  async deleteByIds (ids, options) {
+    return this.getDBFromOptions(options).whereIn('id', ids).delete()
+  }
+
+  async deleteMany (filter, options) {
+    return this.getDBFromOptions(options).where(filter).delete(filter)
+  }
+
+  async findById (id, options) {
+    return this.getDBFromOptions(options).where('id', id)
+  }
+
+  async findByIds (ids, options) {
+    return this.getDBFromOptions(options).whereIn('id', ids)
+  }
+
+  async findOne (filter, options) {
+    return this.getDBFromOptions(options).where(filter).limit(1).first()
+  }
+
+  async find (filter, options) {
+    return this.createCursor(filter, options)
+  }
+
+  async count (filter, options) {
+    return this.getDBFromOptions(options).where(filter).count('id')
+  }
+
+  async transaction (cb) {
+    return this.db.transaction(cb)
+  }
+
+  createCursor (filter = {}, options) {
+    const { $select, $limit, $offset, $sort, ...query } = filter
+    const cursor = this.getDBFromOptions(options).where(query)
+
+    // select
+    if ($select) {
+      cursor.select($select)
+    }
+
+    // limit
+    if ($limit) {
+      cursor.limit($limit)
+    }
+
+    // offset
+    if ($offset) {
+      cursor.offset($offset)
+    }
+
+    // sort
+    if ($sort) {
+      $sort.split(',').forEach(_sort => {
+        if (_sort.indexOf('-') === 0) {
+          cursor.orderBy(_sort.substring(1), 'desc')
+        } else if (_sort.indexOf('+') === 0) {
+          cursor.orderBy(_sort.substring(1), 'asc')
+        } else {
+          cursor.orderBy(_sort.substring(1), 'asc')
+        }
+      })
+    }
+
+    return cursor
+  }
+}
