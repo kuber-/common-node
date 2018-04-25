003c1614fcb3bca99acb36d582532df613f247b4
Kuber Kumar <kuber.kumar@gmail.com>
2018-04-25 16:15:24 +1000

    Add network param for generating btc address.

--- packages/crypto-utils/src/btc.js
+++ packages/crypto-utils/src/btc.js
@@ -1,13 +1,26 @@
-import { ECPair } from 'bitcoinjs-lib'
+import bicoinjs, { ECPair } from 'bitcoinjs-lib'
+
+bicoinjs.networks.regtest = {
+  messagePrefix: '\x18Bitcoin Signed Message:\n',
+  bech32: 'rb',
+  bip32: {
+    public: 0xeab4fa05,
+    private: 0xeab404c7
+  },
+  pubKeyHash: 0x3c,
+  scriptHash: 0x26,
+  wif: 0x5a
+}
 
 /**
  * Generates a new address for Bitcoin.
  * @return {AccountAddress} Newly generated account address.
  */
-const generateAddress = () => {
-  const keyPair = ECPair.makeRandom()
+const generateAddress = (network) => {
+  network = network || 'bitcoin'
+  const keyPair = ECPair.makeRandom({ network: bicoinjs.networks[network] })
+  const privateKey = keyPair.toWIF()
   const address = keyPair.getAddress()
-  var privateKey = keyPair.toWIF()
 
   // address matches /^\w{26,34}$/
   return {
